fb9d3d2619df2cc37f2f331e2ffa5702d3bb60ca
AndriyZyakun <azyakun@gmail.com>
2024-09-13 13:31:23 +0200

    add axios

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import { Home, Cart } from './pages';
 
 import './scss/app.scss';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function App() {
 
   const [pizzas, setPizzas] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:3000/db.json").then((resp) => resp.json()).then((json) => setPizzas(json.pizzas));
+    axios.get("http://localhost:3000/db.json")
+      .then(({ data }) => setPizzas(data.pizzas));
+
+    // fetch("http://localhost:3000/db.json")
+    // .then((resp) => resp.json())
+    // .then((json) => setPizzas(json.pizzas));
   }, []);
 
   return (
